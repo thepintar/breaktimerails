0cd7110023befa2bde4bdae9a5862b9f3d3a2857
Michael Pintar <thepintar@gmail.com>
2016-05-31 13:59:37 -0500

    create functional javascript/jquery timer

--- app/assets/javascripts/timeboxes.js
+++ app/assets/javascripts/timeboxes.js
@@ -1,35 +1,72 @@
-function decrementTime(){
-	while (totalSec > 0){
-		if (seconds == 0){
-			seconds = 59;
-			minutes = minutes - 1;
-			console.log("minutes: " + minutes)
-		}
-		else {
-			seconds = seconds - 1;
-			console.log("seconds: " + seconds)
-			};
-		$("#timer-min").html(minutes);
-		$("#timer-sec").html(seconds);
+var Timer = function(){
+	this.getTime = function(){
+		return $("#timer-div")
 	}
-		clearInterval(timerFunction);
+}
 
+var TimeBox = function(min){
+	console.log("new Timebox")
+	var timer = new Timer();
+	var self = this;
+	this.minutes = min;
+	this.seconds = 0;
+
+	this.decrementTime = function(){
+			console.log("time decrementing")
+			console.log(self.minutes);
+			console.log(self.seconds);
+			if (self.seconds == 0){
+					self.seconds = 59;
+					self.minutes = self.minutes - 1;
+					console.log("minutes: " + self.minutes)
+				}
+				else {
+					self.seconds = self.seconds - 1;
+					console.log("seconds: " + self.seconds)
+				};
+			
+		}
+
+	this.updateDisplay = function (){
+		$("#timer-min").html(self.minutes);
+		if (self.seconds<10){
+			$("#timer-sec").html("0"+self.seconds)
+		} else {
+			$("#timer-sec").html(self.seconds);
+		}
 	}
-function startTimer(){
-	timerFunction = setInterval(decrementTime, 1000);
+
+	this.runTimer = function (){
+			console.log("one second passes")
+			self.updateDisplay();
+			self.decrementTime();
+	}
+
+	this.checkTimeDone = function (){
+			if (self.minutes === 0 && self.seconds === 0){
+				return true;
+			} else {
+				return false;
+			}
+		}
 }
 
 $(document).ready(function(){
-	var minutes = parseInt($("#timer-min").html());
-	var seconds = 0;
-	var totalSec = minutes * 60;
+	var min = parseInt($("#timer-min").html());
+	var totalSec = min * 60;
 
 
-$("#start-button").on("click", function(){
-
-	startTimer();
-	
-
-});
+	$("#start-button").on("click", function(){
+		console.log("clicked new")
+		var intervalID;
+		timer = new TimeBox(min);
+		console.log(timer);
+		// intervalID = 
+		setInterval(timer.runTimer, 1000);
+		// if(timer.checkTimeDone() == true){
+		// 	clearInterval(intervalID);
+		// };
+		
+	});
 
 });
