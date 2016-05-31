b00361b388e7f5e3189277aaf69c6a129f47f173
oanhcodes <linda.oanh.ho@gmail.com>
2016-05-31 12:59:32 -0500

    small commit to merge master

--- app/controllers/timeboxes_controller.rb
+++ app/controllers/timeboxes_controller.rb
@@ -1,6 +1,12 @@
 class TimeboxesController < ApplicationController
 
-	def show
+	def new
+    @timebox = Timebox.new
+  end
+
+  def create
+  end
+  def show
 		@timebox = Timebox.find(params[:id])
 		redirect_to '/' if @timebox.creator != current_user
 	end
