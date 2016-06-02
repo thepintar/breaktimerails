0a363cc9a471dfc5f81a31b40f3a2875f713e56d
oanhcodes <linda.oanh.ho@gmail.com>
2016-06-02 15:56:48 -0500

    Add activity descriptions

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -10,7 +10,7 @@ class ActivitiesController < ApplicationController
   def create
     @activity = Activity.new(activity_params)
     if @activity.save
-      redirect_to '/timeboxes/new'
+      redirect_to '/activities'
     else
       @errors = @activity.errors.full_messages
       flash.notice = "You must add an activity name."
