7fe14e730073c957d1893e5d4d189e687880490f
oanhcodes <linda.oanh.ho@gmail.com>
2016-06-02 13:12:10 -0500

    Add user validations and error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,9 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to 'signup'
+      @errors = user.errors.full_messages
+      flash.notice = "Registration failed. Please try again."
+      render '/users/new'
     end
   end
 
